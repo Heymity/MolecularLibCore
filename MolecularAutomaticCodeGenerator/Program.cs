f33689310c3de6897fb9285305e2cc6b7bd50e2c
Heymity <gabriel.prscavone@gmail.com>
2022-04-02 01:57:09 -0300

    Creating the code generator for InstantiateWithArgs

---
+++ MolecularAutomaticCodeGenerator/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MolecularLib.CodeGenerator;
+
+const string basePath = @"D:\Unity\UnityProjects\MolecularLibCore\Assets\Lib\Core\InstantiateHelpers\";
+
+File.WriteAllText(basePath + InstantiateMethodsCodeCreator.MethodsFileName, InstantiateMethodsCodeCreator.CreateMethodsCode());
+File.WriteAllText(basePath + InstantiateMethodsCodeCreator.InterfacesFileName, InstantiateMethodsCodeCreator.CreateInterfacesCode());
