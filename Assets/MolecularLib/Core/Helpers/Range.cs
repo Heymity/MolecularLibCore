ac9c5ff6c5e91db2d53ab6b23e36c15bf9461784
Heymity <gabriel.prscavone@gmail.com>
2022-04-19 19:08:50 -0300

    Range editor bug fixes

--- Assets/MolecularLib/Core/Helpers/Range.cs
+++ Assets/MolecularLib/Core/Helpers/Range.cs
@@ -7,7 +7,7 @@ namespace MolecularLib.Helpers
     {
         void ValidateMinMaxValues();
     }
-    
+
     public interface IRange<T> : IRange
     {
         T Min { get; set; }
@@ -20,8 +20,17 @@ namespace MolecularLib.Helpers
         [SerializeField] protected T min;
         [SerializeField] protected T max;
 
-        public virtual T Min { get => min; set => min = value; }
-        public virtual T Max { get => max; set => max = value; }
+        public virtual T Min
+        {
+            get => min;
+            set => min = value;
+        }
+
+        public virtual T Max
+        {
+            get => max;
+            set => max = value;
+        }
 
         public virtual bool IsInRange(T value) => value.CompareTo(Min) > 0 && value.CompareTo(Max) < 0;
 
@@ -62,18 +71,21 @@ namespace MolecularLib.Helpers
             this.max = max;
         }
 
-        public Range() : this(default, default) { }
+        public Range() : this(default, default)
+        {
+        }
     }
 
     [Serializable]
     public class Range : Range<float>
     {
         public float MidPoint => (Min + Max) / 2f;
-        
+
         public float Lerp(float t) => Mathf.Lerp(Min, Max, t);
         public float LerpUnclamped(float t) => Mathf.LerpUnclamped(Min, Max, t);
         public float InverseLerp(float value) => Mathf.InverseLerp(Min, Max, value);
         public float InverseLerpUnclamped(float value) => Maths.InvLerp(Min, Max, value);
+
         /// <summary>
         /// Min inclusive, Max inclusive
         /// </summary>
@@ -90,6 +102,7 @@ namespace MolecularLib.Helpers
         public int LerpUnclamped(float t) => Mathf.RoundToInt(Mathf.LerpUnclamped(Min, Max, t));
         public float InverseLerp(int value) => Mathf.InverseLerp(Min, Max, value);
         public float InverseLerpUnclamped(int value) => Maths.InvLerp(Min, Max, value);
+
         /// <summary>
         /// Min inclusive, Max exclusive
         /// </summary>
@@ -103,25 +116,37 @@ namespace MolecularLib.Helpers
         [SerializeField] private Vector3 minVector3SaveData;
         [SerializeField] private Vector3 maxVector3SaveData;
 
-        public new Vector3 Min { get => ToVec3(base.Min); set => base.Min = FromVec3(value); } 
-        public new Vector3 Max { get => ToVec3(base.Max); set => base.Max = FromVec3(value); } 
+        public new Vector3 Min
+        {
+            get => ToVec3(base.Min);
+            set => base.Min = FromVec3(value);
+        }
+
+        public new Vector3 Max
+        {
+            get => ToVec3(base.Max);
+            set => base.Max = FromVec3(value);
+        }
 
         public Vector3 Lerp(float t) => Vector3.Lerp(Min, Max, t);
         public Vector3 LerpUnclamped(float t) => Vector3.LerpUnclamped(Min, Max, t);
         public float InverseLerp(Vector3 value) => Mathf.Clamp01(Maths.InvLerp(Min, Max, value));
         public float InverseLerpUnclamped(Vector3 value) => Maths.InvLerp(Min, Max, value);
-        public Vector3 Random() => new Vector3(UnityEngine.Random.Range(Min.x, Max.x), UnityEngine.Random.Range(Min.y, Max.y), UnityEngine.Random.Range(Min.z, Max.z));
+
+        public Vector3 Random() => new Vector3(UnityEngine.Random.Range(Min.x, Max.x),
+            UnityEngine.Random.Range(Min.y, Max.y), UnityEngine.Random.Range(Min.z, Max.z));
 
         public bool IsInRange(Vector3 vec3) => Maths.IsAllGreaterThan(Min, vec3) && Maths.IsAllSmallerThan(Max, vec3);
 
         public Bounds GetBoundingBox()
         {
-            var center = new Vector3((Max.x - Min.x) / 2 + Min.x, (Max.y - Min.y) / 2 + Min.y, (Max.z - Min.z) / 2 + Min.z);
+            var center = new Vector3((Max.x - Min.x) / 2 + Min.x, (Max.y - Min.y) / 2 + Min.y,
+                (Max.z - Min.z) / 2 + Min.z);
             var size = new Vector3(Max.x - Min.x, Max.y - Min.y, Max.z - Min.z);
 
             return new Bounds(center, size);
         }
-        
+
         public Vector3 Clamp(Vector3 vec3)
         {
             if (vec3.x < Min.x) vec3.x = Min.x;
@@ -153,7 +178,9 @@ namespace MolecularLib.Helpers
             return vec3;
         }
 
-        public static Vector3 ToVec3((float x, float y, float z) vec3Tup) => new Vector3(vec3Tup.x, vec3Tup.y, vec3Tup.z);
+        public static Vector3 ToVec3((float x, float y, float z) vec3Tup) =>
+            new Vector3(vec3Tup.x, vec3Tup.y, vec3Tup.z);
+
         public static (float x, float y, float z) FromVec3(Vector3 vec3) => (vec3.x, vec3.y, vec3.z);
 
         public void OnBeforeSerialize()
@@ -161,6 +188,7 @@ namespace MolecularLib.Helpers
             minVector3SaveData = Min;
             maxVector3SaveData = Max;
         }
+
         public void OnAfterDeserialize()
         {
             Min = minVector3SaveData;
@@ -174,14 +202,25 @@ namespace MolecularLib.Helpers
         [SerializeField] private Vector2 minVector2SaveData;
         [SerializeField] private Vector2 maxVector2SaveData;
 
-        public new Vector2 Min { get => ToVec2(base.Min); set => base.Min = FromVec2(value); }
-        public new Vector2 Max { get => ToVec2(base.Max); set => base.Max = FromVec2(value); }
+        public new Vector2 Min
+        {
+            get => ToVec2(base.Min);
+            set => base.Min = FromVec2(value);
+        }
+
+        public new Vector2 Max
+        {
+            get => ToVec2(base.Max);
+            set => base.Max = FromVec2(value);
+        }
 
         public Vector2 Lerp(float t) => Vector2.Lerp(Min, Max, t);
         public Vector2 LerpUnclamped(float t) => Vector2.LerpUnclamped(Min, Max, t);
         public float InverseLerp(Vector2 value) => Mathf.Clamp01(Maths.InvLerp(Min, Max, value));
         public float InverseLerpUnclamped(Vector2 value) => Maths.InvLerp(Min, Max, value);
-        public Vector2 Random() => new Vector2(UnityEngine.Random.Range(Min.x, Max.x), UnityEngine.Random.Range(Min.y, Max.y));
+
+        public Vector2 Random() =>
+            new Vector2(UnityEngine.Random.Range(Min.x, Max.x), UnityEngine.Random.Range(Min.y, Max.y));
 
         public bool IsInRange(Vector2 vec2) => Maths.IsAllGreaterThan(Min, vec2) && Maths.IsAllSmallerThan(Max, vec2);
 
@@ -192,7 +231,7 @@ namespace MolecularLib.Helpers
 
             return new Bounds(center, size);
         }
-        
+
         public Vector2 Clamp(Vector2 vec2)
         {
             if (vec2.x < Min.x) vec2.x = Min.x;
@@ -228,6 +267,7 @@ namespace MolecularLib.Helpers
             minVector2SaveData = Min;
             maxVector2SaveData = Max;
         }
+
         public void OnAfterDeserialize()
         {
             Min = minVector2SaveData;
@@ -235,6 +275,83 @@ namespace MolecularLib.Helpers
         }
     }
     
+    [Serializable]
+    public class RangeVector2Int : Range<(int x, int y)>, ISerializationCallbackReceiver
+    {
+        [SerializeField] private Vector2Int minVector2SaveData;
+        [SerializeField] private Vector2Int maxVector2SaveData;
+
+        public new Vector2Int Min
+        {
+            get => ToVec2(base.Min);
+            set => base.Min = FromVec2(value);
+        }
+
+        public new Vector2Int Max
+        {
+            get => ToVec2(base.Max);
+            set => base.Max = FromVec2(value);
+        }
+
+        public float InverseLerp(Vector2Int value) => Mathf.Clamp01(Maths.InvLerp(Min, Max, value));
+        public float InverseLerpUnclamped(Vector2Int value) => Maths.InvLerp(Min, Max, value);
+
+        public Vector2Int Random() =>
+            new Vector2Int(UnityEngine.Random.Range(Min.x, Max.x), UnityEngine.Random.Range(Min.y, Max.y));
+
+        public bool IsInRange(Vector2Int vec2) => Maths.IsAllGreaterThan(Min, vec2) && Maths.IsAllSmallerThan(Max, vec2);
+
+        public Bounds GetBoundingBox()
+        {
+            var center = new Vector3((Max.x - Min.x) / 2 + Min.x, (Max.y - Min.y) / 2 + Min.y);
+            var size = new Vector3(Max.x - Min.x, Max.y - Min.y);
+
+            return new Bounds(center, size);
+        }
+
+        public Vector2Int Clamp(Vector2Int vec2)
+        {
+            if (vec2.x < Min.x) vec2.x = Min.x;
+            if (vec2.y < Min.y) vec2.y = Min.y;
+
+            if (vec2.x > Max.x) vec2.x = Max.x;
+            if (vec2.y > Max.y) vec2.y = Max.y;
+
+            return vec2;
+        }
+
+        public Vector2Int ClampCeil(Vector2Int vec2)
+        {
+            if (vec2.x > Max.x) vec2.x = Max.x;
+            if (vec2.y > Max.y) vec2.y = Max.y;
+
+            return vec2;
+        }
+
+        public Vector2Int ClampFloor(Vector2Int vec2)
+        {
+            if (vec2.x < Min.x) vec2.x = Min.x;
+            if (vec2.y < Min.y) vec2.y = Min.y;
+
+            return vec2;
+        }
+
+        public static Vector2Int ToVec2((int x, int y) vec2Tup) => new Vector2Int(vec2Tup.x, vec2Tup.y);
+        public static (int x, int y) FromVec2(Vector2Int vec2) => (vec2.x, vec2.y);
+
+        public void OnBeforeSerialize()
+        {
+            minVector2SaveData = Min;
+            maxVector2SaveData = Max;
+        }
+
+        public void OnAfterDeserialize()
+        {
+            Min = minVector2SaveData;
+            Max = maxVector2SaveData;
+        }
+    }
+
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
     public class MinMaxRangeAttribute : PropertyAttribute
     {
@@ -251,7 +368,10 @@ namespace MolecularLib.Helpers
 
     public static class RangeExtensionMethods
     {
-        public static float Random(this Range<float> range) => (range as Range)?.Random() ?? throw new NullReferenceException();
-        public static float Random(this Range<int> range) => (range as RangeInteger)?.Random() ?? throw new NullReferenceException();
+        public static float Random(this Range<float> range) =>
+            (range as Range)?.Random() ?? throw new NullReferenceException();
+
+        public static float Random(this Range<int> range) =>
+            (range as RangeInteger)?.Random() ?? throw new NullReferenceException();
     }
 }
