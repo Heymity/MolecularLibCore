5f1f2a496489b8bb01f6b270b27994041a8cfd34
Heymity <gabriel.prscavone@gmail.com>
2022-04-21 22:21:53 -0300

    Demo scene

--- Assets/HandsomeDynosaur/MolecularLib/Core/Demo/DemoDrawersScript.cs
+++ Assets/HandsomeDynosaur/MolecularLib/Core/Demo/DemoDrawersScript.cs
@@ -2,15 +2,19 @@ using System;
 using System.Collections.Generic;
 using MolecularLib.Helpers;
 using MolecularLib.PolymorphismSupport;
+using MolecularLib.Testing;
 using UnityEngine;
 
-namespace MolecularLib.Testing
+namespace MolecularLib
 {
-    public class Tests : MonoBehaviour
+    public class DemoDrawersScript : MonoBehaviour
     {
-        [SerializeField] private Sprite sprite; 
+        [Header("Sprite, Tag and Type variable editor drawers")] 
+        [SerializeField] private Sprite sprite;
         [SerializeField] private Tag tagTest;
-        [Space]
+        [SerializeField, TypeVariableBaseType(typeof(MonoBehaviour))] private TypeVariable type;
+        [Space] 
+        [Header("Range variables and editor drawers")] 
         [SerializeField] private Range<double> doubleRange;
         [SerializeField] private Range<float> genericFloatRange;
         [SerializeField] private Range floatRange1;
@@ -18,25 +22,26 @@ namespace MolecularLib.Testing
         [SerializeField, MinMaxRange(-30, 20)] private RangeInteger intRange;
         [SerializeField] private RangeVector2 vec2Range;
         [SerializeField] private RangeVector3 vec3Range;
-        [Space]
-        [SerializeField, TypeVariableBaseType(typeof(MonoBehaviour))] private TypeVariable type;
-        [Space]
+        [Space] 
+        [Header("Serializable Dictionary examples")] 
         [SerializeField] private SerializableDictionary<string, int> stringToInt;
         [SerializeField] private SerializableDictionary<HideFlags, Color> flagsToColor;
         [SerializeField] private SerializableDictionary<string, TestStruct> myStructs;
         [SerializeField] private SerializableDictionary<TestStruct, string> myStructsOpposite;
         [SerializeField] private SerializableDictionary<TestStruct, TestStruct> myStructsBoth;
+        //TODO this is not working
         [SerializeField] private SerializableDictionary<string, Sprite> stringToSprite;
-        [Space]
+        [Space] 
+        [Header("Optional variable examples")] 
         [SerializeField] private Optional<string> myOptionalString;
         [SerializeField] private Optional<List<string>> myList;
         [SerializeField] private Optional<SerializableDictionary<string, string>> myOptionalDictionary;
-        //TODO Fix this: [SerializeField] private Optional<Range> myOptionalRange;
-        //[Space]
+        [Space] 
+        [Header("Polymorphic variable examples")] 
         [SerializeField] private PolymorphicVariable<Base> myPolymorphicVariable;
 
-        /*[ContextMenu("Test Polymorphic Variable")]
-        private void TestPoly()
+        [ContextMenu("Test Polymorphic Variable")]
+        public void TestPoly()
         {
             if (myPolymorphicVariable.As<A>(out var asA))
                 Debug.Log($"As A | aClassInt: {asA.aClassInt}");
@@ -46,8 +51,42 @@ namespace MolecularLib.Testing
                 Debug.Log($"As C | cClassFloat: {asC.cClassFloat}");
             else
                 Debug.Log($"As Base | myBaseString: {myPolymorphicVariable.Value.myBaseString}");
-            
+
             myPolymorphicVariable.Value.myBaseString = "Hey, I changed it!";
-        }*/
+        }
+    }
+
+    [Serializable]
+    public struct TestStruct
+    {
+        public int MyInt;
+        public bool MyBool;
+        public List<string> MyStringList;
+    }
+        
+    [Serializable]
+    public class Base
+    {
+        public string myBaseString;
+    }
+
+    [Serializable]
+    public class A : Base
+    {
+        public int aClassInt;
+    }
+
+    [Serializable]
+    public class B : Base
+    {
+        //public Range bClassRange;
+        public int bClassInt;
+    }
+
+    [Serializable]
+    public class C : B
+    {
+        public float cClassFloat;
+        //public TestVolatileScriptableObject cClassScriptableObject;
     }
 }
